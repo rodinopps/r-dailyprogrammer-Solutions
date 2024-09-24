c1fc077ab13cf00923a6fdf8c903dc3efd599c2c
Rodin Opuz <opuzrodin@gmail.com>
2024-09-24 15:56:42 +0100

    Add files via upload

---
+++ Python/Easy/easy-challenge-001.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+reddit = input("Enter your reddit username: ")
+
+print(f"your name is {name}, you are {age} years old, and your username is {reddit}")
