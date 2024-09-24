a3b83fe23c937d03f19af57cdda6975d1ed77033
Rodin Opuz <opuzrodin@gmail.com>
2024-09-24 16:01:38 +0100

    Update easy-challenge-001.py

--- Python/Easy/easy-challenge-001.py
+++ Python/Easy/easy-challenge-001.py
@@ -2,4 +2,7 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 reddit = input("Enter your reddit username: ")
 
-print(f"your name is {name}, you are {age} years old, and your username is {reddit}")
+with open('info.txt', 'w') as file:
+    file.write(f"Your name is {name}, you are {age} years old, and your username is {reddit}\n")
+
+print("Info has been saved to a text file:")
